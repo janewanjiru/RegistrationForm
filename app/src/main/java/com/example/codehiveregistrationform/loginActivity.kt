08af0d028a8717bf9ea52376d4a8b601753194e7
Jane@Wanjiru2021 <Jane@Wanjiru2021>
2021-07-25 11:16:10 +0300

    kotlin

--- app/src/main/java/com/example/codehiveregistrationform/loginActivity.kt
+++ app/src/main/java/com/example/codehiveregistrationform/loginActivity.kt
@@ -3,9 +3,9 @@ package com.example.codehiveregistrationform
 import androidx.appcompat.app.ppCompatActivity
 import android.os.Bundle
 
-class loginActivity : AppCompatActivity(loginRequest:LoginRequest) :AppCompatActivity()  {
+class loginActivity : AppCompatActivity(loginRequest){
     lateinit var email: EditText
-    lateinit var etpassword: EditText
+//    lateinit var etpassword: EditText
     lateinit var btnlogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +14,7 @@ class loginActivity : AppCompatActivity(loginRequest:LoginRequest) :AppCompatAct
 
     fun setViews() {
         email = findViewById(R.id.etemmail)
-        etpassword = findViewById(R.id.etpasswrd)
+//        etpassword = findViewById(R.id.etpasswrd)
         btnlogin = findViewById(R.id.btnlogin)
     }
 
@@ -25,17 +25,18 @@ class loginActivity : AppCompatActivity(loginRequest:LoginRequest) :AppCompatAct
             if (etemail.isEmpty())
                 email.setError("Email required")
 
-            var etpassword = email.text.toString()
-            if (etpassword.isEmpty())
-                email.setError("Password required")
+//            var etpassword = email.text.toString()
+//            if (etpassword.isEmpty())
+//                email.setError("Password required")
 
         }
         var logoinRequest = LoginRequest(
             email = "Email",
-            password = "password"
+//            password = "password"
         )
 
     }
+
     var retrofit = ApiClient.buildApiClient(ApiInteface::class.java)
     var request = retrofit.logiStudent(loginRequest).enqueue(object : Callback<LoginResponce> {
         override fun onResponse(call: Call<LoginResponce>, response: Response<LoginResponce>) {
@@ -43,10 +44,9 @@ class loginActivity : AppCompatActivity(loginRequest:LoginRequest) :AppCompatAct
                 Toast.makeText(baseContext, "Login Successful", Toast.LENGTH_LONG)
                     .show()
         }
+
         override fun onFailure(call: Call<LoginResponce>, t: Throwable) {
             Toast.makeText(baseContext, t.message, Toast.LENGTH_LONG).show()
         }
     })
-}
-
 }
